7695d5cc103d64f779cefb58f6d89314fd0e8dd2
Serhii [boonya] Buinytskyi <boonya41@gmail.com>
2020-07-06 12:52:55 +0300

    Example file has adjusted

--- src/example.ts
+++ src/example.ts
@@ -1,28 +1,54 @@
+import readline from 'readline';
 import Recorder, { RecorderEvents } from './recorder';
 
+const log = (event: string) => (...args: any[]) => {
+  console.log(`${new Date().toISOString().split('T').join(' ')} :: Event "${event}": `, ...args);
+};
+
+readline.emitKeypressEvents(process.stdin);
+if (process.stdin.isTTY) {
+  process.stdin.setRawMode(true);
+}
+
 try {
-  new Recorder(
+  const recorder = new Recorder(
     'rtsp://192.168.0.100:554/user=admin_password=tlJwpbo6_channel=1_stream=0.sdp?real_stream',
     'dist/Recorder',
     {
-      title: 'Test Camera',
+      title: 'Test cam',
       segmentTime: '10m',
       directoryPattern: '%Y.%m.%d',
       filenamePattern: '%H.%M.%S-test-cam',
       dirSizeThreshold: '20G',
       autoClear: true,
     },
-  ).on(RecorderEvents.STARTED, (...args) => console.log(`${RecorderEvents.STARTED}: `, ...args))
-    .on(RecorderEvents.STOPPED, (...args) => console.log(`${RecorderEvents.STOPPED}: `, ...args))
-    .on(RecorderEvents.ERROR, (...args) => console.log(`${RecorderEvents.ERROR}: `, ...args))
-    .on(RecorderEvents.SEGMENT_STARTED, (...args) => console.log(`${RecorderEvents.SEGMENT_STARTED}: `, ...args))
-    .on(RecorderEvents.FILE_CREATED, (...args) => console.log(`${RecorderEvents.FILE_CREATED}: `, ...args))
-    .on(RecorderEvents.DIRECTORY_CREATED, (...args) => console.log(`${RecorderEvents.DIRECTORY_CREATED}: `, ...args))
-    .on(RecorderEvents.STOP, (...args) => console.log(`${RecorderEvents.STOP}: `, ...args))
-    // .on(RecorderEvents.PROGRESS, (buffer: Buffer) => console.log(`${RecorderEvents.PROGRESS}: `, buffer.toString()))
-    .on(RecorderEvents.SPACE_FULL, (...args) => console.log(`${RecorderEvents.SPACE_FULL}: `, ...args))
-    .on(RecorderEvents.SPACE_WIPED, (...args) => console.log(`${RecorderEvents.SPACE_WIPED}: `, ...args))
-    .start();
+  );
+
+  recorder.on(RecorderEvents.STARTED, log(RecorderEvents.STARTED))
+    .on(RecorderEvents.STOPPED, log(RecorderEvents.STOPPED))
+    .on(RecorderEvents.ERROR, log(RecorderEvents.ERROR))
+    .on(RecorderEvents.SEGMENT_STARTED, log(RecorderEvents.SEGMENT_STARTED))
+    .on(RecorderEvents.FILE_CREATED, log(RecorderEvents.FILE_CREATED))
+    .on(RecorderEvents.DIRECTORY_CREATED, log(RecorderEvents.DIRECTORY_CREATED))
+    .on(RecorderEvents.STOP, log(RecorderEvents.STOP))
+    // .on(RecorderEvents.PROGRESS, log(RecorderEvents.PROGRESS))
+    .on(RecorderEvents.SPACE_FULL, log(RecorderEvents.SPACE_FULL))
+    .on(RecorderEvents.SPACE_WIPED, log(RecorderEvents.SPACE_WIPED));
+
+  process.stdin.on('keypress', (_, key) => {
+    if (key.ctrl && key.name === 'c') {
+      if (recorder.isRecording()) {
+        recorder.stop();
+      }
+      process.exit();
+    } else if (key.name === 'space') {
+      recorder.isRecording()
+        ? recorder.stop()
+        : recorder.start();
+    }
+  });
+  console.log('Press "space" to start/stop recording, "ctrl + c" to stop a process.');
+  console.log();
 } catch (err) {
   console.error(err.message, { err });
 }
