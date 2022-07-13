a593657af262429faa803521b6c09db7344728db
Serhii [boonya] Buinytskyi <779184+boonya@users.noreply.github.com>
2022-07-14 00:13:11 +0200

    Revert "Just for test"
    This reverts commit 7c41ad754799b277f9196913219af60fa5778352.

--- dist/error.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class RecorderError extends Error {
-    constructor(message?: string);
-}
-export declare class RecorderValidationError extends RecorderError {
-    errors: string[];
-    constructor(message: string, errors?: string[]);
-}
-//# sourceMappingURL=error.d.ts.map
--- dist/helpers/segmentTime.d.ts
+++
@@ -1,3 +0,0 @@
-import { SegmentTimeOption } from '../types';
-export default function transformSegmentTime(value: SegmentTimeOption): number;
-//# sourceMappingURL=segmentTime.d.ts.map
